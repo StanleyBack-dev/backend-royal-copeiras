93e3598f32547536340716a2238810496255a1f0
StanleyBack-dev <stanleyrodrigues504@gmail.com>
2026-04-12 04:14:26 -0300

    chore: update eslint and typescript-eslint dependencies; add eslint configuration file

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    rules: {
+      // Adicione regras personalizadas aqui, se necessário
+    },
+  },
+];
